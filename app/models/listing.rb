ed51ca6cc95debacbb093d14f48b165b5f20cc6e
apradhn <abhijit.pradhan6@gmail.com>
2015-07-09 18:05:11 -0400

    cleared validation and callback specs for listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,30 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  # Validations
+  validates :address, :listing_type, :title, :description, :price, :neighborhood, presence: true
+
+  # Callback registrations
+  after_create :change_user_host_status_to_true
+  after_destroy :change_user_host_status_to_false
+
+  private
+  def change_user_host_status_to_true
+    host = self.host
+    host.host = true
+    host.save
+  end
+
+  def any_listings?
+    self.host.listings.length == 0
+  end
+
+  def change_user_host_status_to_false
+    if any_listings?
+      host = self.host
+      host.host = false
+      host.save
+    end
+  end
 end
