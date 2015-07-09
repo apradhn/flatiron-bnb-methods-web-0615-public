cdf8440d1971f4ea6ff8885818e0a1fa2c163aba
apradhn <abhijit.pradhan6@gmail.com>
2015-07-09 17:44:47 -0400

    cleared city spec

--- app/helpers/cities_helper.rb
+++ app/helpers/cities_helper.rb
@@ -16,14 +16,21 @@ module CitiesHelper
   module ClassMethods
     def highest_ratio_res_to_listings
       # knows the city with the highest ratio of reservations to listings
-
       ratios = self.all.each_with_object({}) do |city, hash|
-        data = self.includes(:reservations, :listings)
-        reservations_count = data.group(:reservations).having("cities.name = ?", city.name)
-        listings_count = data.group(:listings).having("cities.name = ?", city.name)
-        hash[city.name] = reservations_count / listings_count
-        binding.pry
+        reservations_count = city.reservations.size
+        listings_count = city.listings.size
+        hash[city.name] = reservations_count.to_f / listings_count.to_f
       end
+      highest_ratio = ratios.values.max
+      city = ratios.find{|city, ratio| ratio == highest_ratio}.first
+      self.find_by(name: city)
     end
+
+    def most_res
+      # knows the city with the most reservations
+      most_reservations = City.all.collect{|city| city.reservations.size}.max
+      City.find{|city| city.reservations.size == most_reservations}
+    end    
   end
+
 end
